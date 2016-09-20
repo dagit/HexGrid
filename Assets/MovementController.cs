d863cf57b23cb8f0b7143d270acfdf70081245cd
Jason Dagit <dagitj@gmail.com>
2016-09-19 18:19:52 -0700

    Add mouse look

---
+++ Assets/MovementController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+
+    [SerializeField]
+
+    float rotationX = 0f;
+    float rotationY = 0f;
+
+    [SerializeField]
+    float sensitivityX = 500f;
+    [SerializeField]
+    float sensitivityY = 500f;
+
+    float clampAngle = 90.0f;
+
+    bool mouseLock = false;
+
+    void Start()
+    {
+        transform.position = Camera.main.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Comma))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward);
+            //Debug.Log("walk forward: " + pos);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.TransformDirection(Vector3.left);
+            //Debug.Log("strafe left: " + pos);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.E))
+        {
+            transform.position += transform.TransformDirection(Vector3.right);
+            //Debug.Log("strafe right: " + pos);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.O))
+        {
+            transform.position += transform.TransformDirection(Vector3.back);
+            //Debug.Log("walk backwards: " + pos);
+        }
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            mouseLock = !mouseLock;
+        }
+
+        if (!mouseLock)
+        {
+
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX * Time.deltaTime;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY * Time.deltaTime;
+
+            rotationY = Mathf.Clamp(rotationY, -clampAngle, clampAngle);
+
+            Quaternion localRotation = Quaternion.Euler(-rotationY, rotationX, 0.0f);
+            transform.rotation = localRotation;
+        }
+
+        Camera.main.transform.position = transform.position;
+        Camera.main.transform.rotation = transform.rotation;
+	}
+}
