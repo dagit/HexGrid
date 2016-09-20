9a702800de5ade59c8966bc8a017aaf5a3ea69f4
Jason Dagit <dagitj@gmail.com>
2016-09-19 18:36:15 -0700

    make movement speed configurable

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -12,6 +12,8 @@ public class MovementController : MonoBehaviour {
     float sensitivityX = 500f;
     [SerializeField]
     float sensitivityY = 500f;
+    [SerializeField]
+    float walkSpeed = 10f;
 
     float clampAngle = 90.0f;
 
@@ -26,22 +28,22 @@ public class MovementController : MonoBehaviour {
     void LateUpdate() {
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Comma))
         {
-            transform.position += transform.TransformDirection(Vector3.forward);
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * walkSpeed;
             //Debug.Log("walk forward: " + pos);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += transform.TransformDirection(Vector3.left);
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * walkSpeed;
             //Debug.Log("strafe left: " + pos);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.E))
         {
-            transform.position += transform.TransformDirection(Vector3.right);
+            transform.position += transform.TransformDirection(Vector3.right) * Time.deltaTime * walkSpeed;
             //Debug.Log("strafe right: " + pos);
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.O))
         {
-            transform.position += transform.TransformDirection(Vector3.back);
+            transform.position += transform.TransformDirection(Vector3.back) * Time.deltaTime * walkSpeed;
             //Debug.Log("walk backwards: " + pos);
         }
         if (Input.GetKeyDown(KeyCode.Tab))
